236a8ce6a702bb40883e5183bed4d1338e186009
IlanVaks <ilanvaks@gmail.com>
2023-05-10 17:01:06 -0400

    Added-More-CSS

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,9 +1,32 @@
+import { Container, Row, Col } from "react-bootstrap";
+import "../styles/page2.css"
+
 export default function Page2 () {
 
+  const imgRoot = process.env.PUBLIC_URL + "/images/"
+
   return ( 
-    <>
-    <h2>Home</h2>
-    
-    </>
+    <section>
+      <Container>
+        <Row>
+          <Col><button class="pg2-button button1">X axis</button></Col>
+          <Col><button class="pg2-button button2">Y axis</button></Col>
+          <Col><button class="pg2-button button3">Scale</button></Col>
+          
+        </Row>
+        <Row>
+          <Col>
+            <div className="profile">
+              <img src={imgRoot+"pict.webp"} alt="profile" />
+
+              <div className="profile-text">
+                <h3>Ticket to Mars</h3>
+                <a href="#" class="profile-button" rel="noreferrer">Buy Now!</a>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
